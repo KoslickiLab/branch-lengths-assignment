c1d9940a3498bdbefe736e5eb15e5bcb04930e1d
raquellewei <raquelle70679@gmail.com>
2024-01-22 10:38:50 -0500

    modified solver and make_lin_input

--- scripts/make_lin_sys_input.py
+++ scripts/make_lin_sys_input.py
@@ -2,25 +2,26 @@ import sys
 sys.path.append('../src')
 import trees as tr
 import argparse
-from scipy.sparse import save_npz
+from scipy import sparse
 import numpy as np
 
 
 def main():
     parser = argparse.ArgumentParser(description="Produce matrix A, y and edges")
     parser.add_argument('-t', '--tree_file', type=str, help="tree file")
-    parser.add_argument('-od', '--out_dir', type=str, help="outdir", default="../data/matlab_input")
+    parser.add_argument('-od', '--out_dir', type=str, help="outdir", default="../data/lin_sys_input")
     parser.add_argument('-o', '--out_prefix', type=str, help="output files basename")
+    parser.add_argument('-p', '--perturb', action='store_true', default=False)
 
     args = parser.parse_args()
 
     tree = tr.read_edge_list(args.tree_file)
-    pw_dist_matrix, leaf_nodes = tr.make_distance_matrix(tree)
+    pw_dist_matrix, leaf_nodes = tr.make_distance_matrix(tree, args.perturb)
     A, y, edges = tr.make_matrix_A(tree, pw_dist_matrix, leaf_nodes)
     np.save(f"{args.out_dir}/{args.out_prefix}_y.npy", y)
     np.save(f"{args.out_dir}/{args.out_prefix}_basis.npy", edges)
-    save_npz(f"{args.out_dir}/{args.out_prefix}_pw_distance.npz", pw_dist_matrix)
-    save_npz(f"{args.out_dir}/{args.out_prefix}_A.npz", A)
+    np.savez(f"{args.out_dir}/{args.out_prefix}_pw_distance.npz", pw_dist_matrix)
+    sparse.save_npz(f"{args.out_dir}/{args.out_prefix}_A.npz", sparse.csr_matrix(A))
 
 
 
--- scripts/solve_branch_lengths.py
+++ scripts/solve_branch_lengths.py
@@ -1,10 +1,12 @@
 import sys
 import argparse
 import numpy as np
+from scipy import sparse
 sys.path.append('..')
 sys.path.append('../src')
 import trees as tr
 import solvers as solvers
+
 
 
 def main():
@@ -12,22 +14,38 @@ def main():
     parser.add_argument('-t', '--tree', help="Tree file")
     parser.add_argument('-A', '--A', help="A matrix file")
     parser.add_argument('-pd', '--pairwise_distance', help="PW distance file")
+    parser.add_argument('-y', '--y', help="y vector file")
+    parser.add_argument('-b', '--basis', help="Basis file")
     parser.add_argument('-o', '--outfile_name', help="Output file name.")
+    parser.add_argument('-m', '--method', help="solving method", choices=['nnls', 'bottom-up'])
+    parser.add_argument('-i', '--iter_num', help="Iteration number", default=1)
 
     args = parser.parse_args()
 
     tree = tr.read_edge_list(args.tree)
     if not args.pairwise_distance:
         pw_matrix, labels = tr.make_distance_matrix(tree)
-    #if not args.A:
-    #    A, y, edges = tr.make_matrix_A(tree, pw_matrix, labels)
+    else:
+        pw_matrix = np.load(args.pairwise_distance)
+        labels = np.load(args.basis)
+    if not args.A:
+        A, y, edges = tr.make_matrix_A(tree, pw_matrix, labels)
+    else:
+        A = sparse.load_npz(args.A)
+        y = np.load(args.y)
+        edges = np.load(args.basis)
+        print(edges)
+        edges = list(map(tuple, edges))
+        print(edges)
+    y = np.asarray(y.T)[0]
     solver = solvers.BranchLengthSolver()
-    #y = np.asarray(y.T)[0]
-    #x = solver.lsq_solver(A, y)
-    #tr.save_edge_lengths_solution(edges, dict(zip(edges, x)), args.outfile_name)
-    solution = solver.deterministic_solver(tree, pw_matrix, labels)
-    print(solution)
-
+    if args.method == 'nnls':
+        x = solver.lsq_solver(A.todense(), y)
+        solution = dict(zip(edges, x))
+        tr.save_edge_lengths_solution(edges, solution, args.outfile_name)
+    else:
+        solution = solver.deterministic_solver(tree, pw_matrix, labels)
+        tr.save_edge_lengths_solution(edges, solution, args.outfile_name)
 
 
 main()
