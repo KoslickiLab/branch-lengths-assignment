559d0d7aa14192e8560b7fb75df3881f6743b91a
raquellewei <raquelle70679@gmail.com>
2024-07-20 21:55:33 -0400

    add test2

--- README.md
+++ README.md
@@ -2,13 +2,26 @@
 ```bash
 git clone https://github.com/KoslickiLab/branch-lengths-assignment.git
 cd branch-lengths-assignment
+pip install -r requirements.txt
 ```
 
-## 1. Experiment 1: recovering branch lengths using compatible distance matrices
+## Experiment 1: recovering branch lengths using compatible distance matrices
 The tests in this part corresponds to section III.B of the manuscript. There are two tests in this section. The first evaluates the performance of bottom-up, naive NNLS and regularized NNLS methods in restoring branch lengths in the event when the distance matrix is completely compatible with the tree. 
 ```bash
 mkdir -p data/test_data/test1
 cd test_scripts
-bash test1_solve_branch_lengths.sh
+bash test1_perfect_scenario_method_performance.sh
+bash test1_selection_factor_effect.sh
 cd ..
 ```
+
+## Experiment 2: recovering branch lengths using pairwise distances with errors
+The tests in this part corresponds to section III.C of the manuscript. 
+```bash
+mkdir -p data/test_data/test2
+cd test_scripts
+bash test2_distance_matrix_with_errors.sh
+cd ..
+```
+
+
---
+++ test_scripts/test2_distance_matrix_with_errors.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -e
+
+cd ../scripts
+
+#generate trees
+output_dir='../data/test_data/test2'
+for r in {3,5,7}
+do
+  for i in {1..10}
+  do
+    file_name="n1000_r"$r"_regular_"$i".txt"
+    echo "$file_name"
+    python generate_tree.py -n 1000 -r $r -od $output_dir/ -o $file_name
+  done
+done
+
+#generate necessary inputs
+for p in 0.01 0.05 0.1 0.2 0.4 0.6 0.8
+do
+  for s in 0.1 0.2 0.4 0.5 0.7 0.9
+  do
+    for f in $output_dir/*.txt
+    do
+    base_name=$(basename $f .txt)"_perturbed"
+    echo $base_name
+    python make_lin_sys_input.py -t $f -od $output_dir -p $p -s $s
+    done
+  done
+done
+
+#generate output
+mkdir "$output_dir"/results
+input_dir=$1
+for tree in "$input_dir"/*.txt
+do
+  echo $tree
+  base_name=$(basename $tree .txt)
+  for p in 0.01 0.05 0.1 0.2 0.4 0.6 0.8
+  do
+    for s in 0.1 0.2 0.4 0.5 0.7 0.9
+    do
+       A_file="$input_dir"/"$base_name"_perturb-"$p"_threshold-"$s"_A.npz
+       y_file="$input_dir"/"$base_name"_perturb-"$p"_threshold-"$s"_y.npy
+       edge_file="$input_dir"/"$base_name"_perturb-"$p"_threshold-"$s"_edges.npy
+       pd="$input_dir"/"$base_name"_perturb-"$p"_threshold-"$s"_pw-distance.npz
+       basis_file="$input_dir"/"$base_name"_perturb-"$p"_threshold-"$s"_leaf-nodes.npy
+       nnls_out_file="$output_dir"/results/"$base_name"_perturb-"$p"_threshold-"$s"_recover_nnls.txt
+       python solve_branch_lengths.py -m nnls -t $tree -A $A_file -y $y_file -l $edge_file -o $nnls_out_file -f 5 -i 100 -pd $pd -b -1 0
+       bu_out_file="$output_dir"/results/"$base_name"_perturb-"$p"_threshold-"$s"_recover_bottom-up.txt
+       python solve_branch_lengths.py -m bottom-up -t $tree -pd $pd -l $basis_file -o $bu_out_file
+    done
+  done
+done
+
+python compare_all_and_plot.py -i ../data/test_data/test2/results -r ../data/test_data/test2/r3 -x "Perturbation proportion" -y "Mean L1 error" -style "Perturbation likelihood" -o ../data/plots/line_perturb_vs_threshold_r3_complete.png -hue Method
+python compare_all_and_plot.py -i ../data/test_data/test2/results -r ../data/test_data/test2/r5 -x "Perturbation proportion" -y "Mean L1 error" -style "Perturbation likelihood" -o ../data/plots/line_perturb_vs_threshold_r5_complete.png -hue Method
+python compare_all_and_plot.py -i ../data/test_data/test2/results -r ../data/test_data/test2/r7 -x "Perturbation proportion" -y "Mean L1 error" -style "Perturbation likelihood" -o ../data/plots/line_perturb_vs_threshold_r7_complete.png -hue Method
+
+
