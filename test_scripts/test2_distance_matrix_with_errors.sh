c1e8fed8feb91721d1825d602add62db9bc537d4
raquellewei <raquelle70679@gmail.com>
2024-07-29 11:22:44 -0400

    add test2

--- test_scripts/test2_distance_matrix_with_errors.sh
+++ test_scripts/test2_distance_matrix_with_errors.sh
@@ -3,35 +3,46 @@ set -e
 
 cd ../scripts
 
-#generate trees
 output_dir='../data/test_data/test2'
+mkdir "$output_dir"/results
+mkdir -p ../data/test_data/test2/r3
+mkdir -p ../data/test_data/test2/r5
+mkdir -p ../data/test_data/test2/r7
+r3_dir="../data/test_data/test2/r3"
+r5_dir="../data/test_data/test2/r5"
+r7_dir="../data/test_data/test2/r7"
+all_input_dir=($r3_dir $r5_dir $r7_dir)
+#generate trees
 for r in {3,5,7}
 do
   for i in {1..10}
   do
     file_name="n1000_r"$r"_regular_"$i".txt"
     echo "$file_name"
-    python generate_tree.py -n 1000 -r $r -od $output_dir/ -o $file_name
+    python generate_tree.py -n 1000 -r $r -od $output_dir/r"$r" -o $file_name
   done
 done
 
 #generate necessary inputs
+for dir in "${all_input_dir[@]}"
+do
 for p in 0.01 0.05 0.1 0.2 0.4 0.6 0.8
 do
   for s in 0.1 0.2 0.4 0.5 0.7 0.9
   do
-    for f in $output_dir/*.txt
+    for f in $dir/*.txt
     do
     base_name=$(basename $f .txt)"_perturbed"
     echo $base_name
     python make_lin_sys_input.py -t $f -od $output_dir -p $p -s $s
     done
   done
+  done
 done
 
 #generate output
-mkdir "$output_dir"/results
-input_dir=$1
+for input_dir in "${all_input_dir[@]}"
+do
 for tree in "$input_dir"/*.txt
 do
   echo $tree
@@ -50,6 +61,7 @@ do
        bu_out_file="$output_dir"/results/"$base_name"_perturb-"$p"_threshold-"$s"_recover_bottom-up.txt
        python solve_branch_lengths.py -m bottom-up -t $tree -pd $pd -l $basis_file -o $bu_out_file
     done
+  done
   done
 done
 
